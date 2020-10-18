0218a6f0cda621a94242a8a83bdfd5bc2ffd7888
davidwmartines <d5172@yahoo.com>
2020-10-18 11:54:37 -0500

    Install flask-resize.

--- trogs_app/application.py
+++ trogs_app/application.py
@@ -1,8 +1,15 @@
 
 import views
 from flask import Flask
+import flask_resize
+import os
 
 application = Flask(__name__)
+
+application.config['RESIZE_STORAGE_BACKEND'] = 's3'
+application.config['RESIZE_S3_BUCKET'] = os.environ['AWS_CONTENT_BUCKET']
+
+resize= flask_resize.Resize(application)
 
 def favicon():
     return application.send_static_file('favicon.ico')
