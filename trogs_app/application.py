9ce69f4b9a3a06eb1f2ff387d34794034bc1ab3d
davidwmartines <d5172@yahoo.com>
2020-10-22 14:50:55 -0500

    Move auth route functions to separate module.

--- trogs_app/application.py
+++ trogs_app/application.py
@@ -1,11 +1,8 @@
 import os
 
 import flask_resize
-from authlib.integrations.flask_client import OAuth
-from flask import Flask, redirect, session, url_for
-from six.moves.urllib.parse import urlencode
-
-import views
+from flask import Flask, redirect, session
+import views, auth
 
 application = Flask(__name__)
 application.secret_key = 'Troglodytes42'
@@ -14,60 +11,19 @@ application.config['RESIZE_STORAGE_BACKEND'] = 's3'
 application.config['RESIZE_S3_BUCKET'] = os.environ['AWS_CONTENT_BUCKET']
 resize = flask_resize.Resize(application)
 
-oauth = OAuth(application)
-
-auth0 = oauth.register(
-    'auth0',
-    client_id=os.environ['AUTH0_CLIENT_ID'],
-    client_secret=os.environ['AUTH0_CLIENT_SECRET'],
-    api_base_url=os.environ['AUTH0_API_BASE_URL'],
-    access_token_url=os.environ['AUTH0_ACCESS_TOKEN_URL'],
-    authorize_url=os.environ['AUTH0_AUTHORIZE_URL'],
-    client_kwargs={
-        'scope': 'openid profile email',
-    }
-)
-
-
-@application.route('/login')
-def login():
-    return auth0.authorize_redirect(redirect_uri=url_for('callback', _external=True))
-
-
-@application.route('/callback')
-def callback():
-    auth0.authorize_access_token()
-    resp = auth0.get('userinfo')
-    userinfo = resp.json()
-    #print('userinfo', userinfo)
-
-    session['profile'] = {
-        'user_id': userinfo['sub'],
-        'name': userinfo.get('name'),
-        'email': userinfo.get('email')
-    }
-    return redirect(url_for('home'))
-
-
-@application.route('/logout')
-def logout():
-    session.clear()
-    params = {
-        'returnTo': url_for('home', _external=True),
-        'client_id': os.environ['AUTH0_CLIENT_ID']
-    }
-    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
-
+auth_routes = auth.create_routes(application)
 
 def favicon():
     return application.send_static_file('favicon.ico')
-
 
 application.add_url_rule('/', view_func=views.home)
 application.add_url_rule('/artist/<id>', view_func=views.artist)
 application.add_url_rule('/album/<id>', view_func=views.album)
 application.add_url_rule('/track/<id>', view_func=views.track)
 application.add_url_rule('/favicon.ico', view_func=favicon)
+application.add_url_rule('/login', view_func=auth_routes['login'])
+application.add_url_rule('/callback', view_func=auth_routes['callback'])
+application.add_url_rule('/logout', view_func=auth_routes['logout'])
 
 # run the app.
 if __name__ == "__main__":
