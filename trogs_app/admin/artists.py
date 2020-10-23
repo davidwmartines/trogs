259fcb246990d98bbaed8bdd076171ead81cf072
davidwmartines <d5172@yahoo.com>
2020-10-23 16:02:00 -0500

    Query db and reutrn jsonapi responses.

--- trogs_app/admin/artists.py
+++ trogs_app/admin/artists.py
@@ -1,14 +1,47 @@
+import ids, db
+from boto3.dynamodb.conditions import Key
 
+class Model:
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+
+class Artist(Model):
+    pass
+
+def item_to_artist(item):
+    return Artist(
+        id = item['PK'],
+        name = item['AA_SK'],
+        bio = item.get('Bio', ''),
+        image_url = item.get('ImageURL', '')
+    )
 
 def list_for_owner(owner):
-    return [{
-        'id': 'artist1',
-        'attributes': {
-            'name': 'Artist 1'
-        }
-    }, {
-        'id': 'artist2',
-        'attributes': {
-            'name': 'Artist 2'
-        }
-    }]
+
+    table = db.get_table()
+
+    res = table.query(
+        IndexName="IX_ARTISTS_BY_OWNER",
+        KeyConditionExpression=Key('Owner').eq(owner)
+    )
+
+    return list(map(item_to_artist, res['Items']))
+
+
+def by_id_for_owner(id, owner):
+
+    table = db.get_table()
+
+    res = table.query(
+        IndexName="IX_ARTISTS_BY_OWNER",
+        KeyConditionExpression=Key('Owner').eq(owner)
+        & Key('PK').eq(id)
+    )
+
+    if not len(res['Items']):
+        return None
+
+    return item_to_artist(res['Items'][0])
+
--- trogs_app/api.py
+++ trogs_app/api.py
@@ -1,10 +1,33 @@
 
 from flask import Flask, current_app, jsonify, session
-from werkzeug.exceptions import NotFound, Forbidden, UnprocessableEntity
+from marshmallow import ValidationError, validate
+from marshmallow_jsonapi import fields
+from marshmallow_jsonapi.flask import Relationship, Schema
+from werkzeug.exceptions import Forbidden, NotFound, UnprocessableEntity
 
 import admin.artists
-
 import auth
+
+
+class ArtistSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    owner = fields.Str(required=True)
+    image_url = fields.Str()
+    bio = fields.Str()
+
+    class Meta:
+        type_ = "artist"
+
+
+def J(*args, **kwargs):
+    """Wrapper around jsonify that sets the Content-Type of the response to
+    application/vnd.api+json.
+    """
+    response = jsonify(*args, **kwargs)
+    response.mimetype = "application/vnd.api+json"
+    return response
+
 
 def current_user_email():
     return session['profile']['email']
@@ -13,26 +36,21 @@ def current_user_email():
 @current_app.route('/api/v1/me/artists')
 @auth.requires_auth
 def my_artists():
-
     owner = current_user_email()
-
     artists = admin.artists.list_for_owner(owner)
-    
-    return jsonify({
-        'data': artists
-    })
+    data = ArtistSchema(many=True).dump(artists)
+    return data
 
 
 @current_app.route('/api/v1/me/artists/<id>')
 @auth.requires_auth
 def my_artist_by_id(id):
-    artist = {
-        'id': id,
-        'attributes': {
-            'name': id
-        }
-    }
-    return jsonify({
-        'data': artist
-    })
+    owner = current_user_email()
+    artist = admin.artists.by_id_for_owner(id=id, owner=owner)
+    print('artist', artist)
+    if artist is None:
+        raise NotFound
+
+    data = ArtistSchema().dump(artist)
+    return data
 
